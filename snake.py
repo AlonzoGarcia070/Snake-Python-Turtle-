4709130fe1dabd8dbbf0aa156f636b688e490099
AlonzoGarcia070 <alonzoagarcia73@gmail.com>
2026-05-07 13:26:54 -0500

    22%>

--- snake.py
+++ snake.py
@@ -18,46 +18,34 @@ def playing_area():
     pen.end_fill()
     
 class Head(Turtle):
-  def __init__(self, screen, body):
+  def __init__(self, screen):
     super().__init__()
     self.alive = True
     self.shape("square")
     self.color("green")
     self.pu
     self.goto(0,0)
-    self.direction("up","down","left","right")
+    self.direction = "Right"
+    screen.onkeypress(up, "Up")
+    screen.onkeypress(down, "Down")
+    screen.onkeypress(right, "Right")
+    screen.onkeypress(left, "Left")
     
   def up(self):
-    global player
-    player.setheading(90)
-    if player.ycor()!=240:
-        player.sety(player.ycor()+10)
-        if player.heading==270:
-          player.setheading!=90
+    if self.heading() != -90:
+      self.setheading(90)
 
   def down(self):
-    global player
-    player.setheading(-90)
-    if player.ycor()!=-240:
-      player.sety(player.ycor()-10)
-      if player.heading==-90:
-        player.setheading!=270
-
+    if self.heading() != 90:
+      self.setheading(-90)
+    
   def left(self):
-    global player
-    player.setheading(180)
-    if player.xcor()!=-240:
-        player.setx(player.xcor()-10)
-        if player.heading==180:
-          player.setheading!=0
+    if player.setheading() !=0:
+      self.setheading(180)
 
   def right(self):
-    global player
-    player.setheading(0)
-    if player.xcor()!=240:
-        player.setx(player.xcor()+10)
-        if player.heading==0:
-          player.setheading!=180
+    if player.setheading() !=180:
+      self.setheading(0)
 
   def move(self):
     self.forward(20)
@@ -93,14 +81,14 @@ class Apple(Turtle):
   def relocate(self):
     self.goto(random.randint(-230,230), random.randint(-230,230))
 
-  def update():
-    if head.alive:
-      head.move()
+def update():
+  if head.alive:
+    head.move()
 
-      if head.distance(apple) <20:
-        apple.relocate()
+    if head.distance(apple) <20:
+      apple.relocate()
 
-  # Screen.ontimer(update, 120)
+  screen.ontimer(update, 120)
 
 # while head.alive:
 
@@ -111,17 +99,15 @@ class Apple(Turtle):
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(520,520)
-screen.listen
-screen.onkey(create_player, "space" )
-screen.onkeypress(up, "Up")
-screen.onkeypress(down, "Down")
-screen.onkeypress(right, "Right")
-screen.onkeypress(left, "Left")
-
-# Key Binding. Connects key presses and mouse clicks with function calls
 screen.listen()
+screen.onkey(update, "space")
 
-body = []
+
+
+
+
+head = Head(screen)
+body = [head]
 
 
 
